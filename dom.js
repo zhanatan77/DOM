0fef7bb31a99f82d24b6cdf5e904b39cf430d4df
zhanatan12 <zhanatankurbanbaev66@gmail.com>
2023-07-08 17:22:17 +0600

    asd

---
+++ dom.js
@@ -0,0 +1,74 @@
+// const div = document.getElementsByTagName('div')
+// const text = document.getElementsByClassName('text')
+// const text2 = document.getElementById('text-2')
+
+// const div = document.querySelector('div')
+// const  text = document.querySelector('.text')
+// const text2 = document.querySelector('#text-2')
+
+
+// const block = document.querySelector('div')
+// const className = document.querySelector('.block')
+// const idName = document.querySelector('#block-id')
+// const text = document.querySelector('.text')
+// const btn = document.querySelector('button')
+
+// text.style.fontSize = '80px'
+// btn.style.fontSize = '50px'
+
+// btn.addEventListener('click', () => {
+//     text.style.color = 'red'
+// })
+// text.style.color = 'black'
+// text.style.fontSize = '70px'
+//
+// btn.addEventListener('click', () => {
+//     btn.style.color = 'blue'
+// })
+// btn.style.background = 'blue'
+// btn.style.color = 'aqua'
+
+
+// btn.addEventListener('click', () => {
+//     text.style.transition = '1s'
+//     text.style.rotate = '360deg'
+//     text.style.color = 'red'
+//     text.style.transform = 'rotate(360deg)'
+// })
+// text.addEventListener('click', () => {
+//     text.style.rotate = ''
+//     text.style.color = 'blue'
+// })
+
+
+
+// btn.addEventListener('click', () => {
+//     console.log('hello dom')
+// })
+
+
+
+
+
+
+// btn.addEventListener('click', () => {
+//     text.style.color = 'red'
+// })
+// btn.addEventListener('mouseover', () => {
+//     text.style.color = 'red'
+// })
+//
+// text.addEventListener('mouseleave', () => {
+//     text.style.color = 'red'
+// })
+// btn.addEventListener('mousedown', () => {
+//     text.style.color = 'red'
+// })
+// text.addEventListener('mouseup', () => {
+//     text.style.color = 'red'
+// })
+
+
+
+
+
---
+++ ivent.js
@@ -0,0 +1,44 @@
+//
+// const block = document.querySelector('div')
+// const className = document.querySelector('.block')
+// const idName = document.querySelector('#block-id')
+// const text = document.querySelector('.text')
+// const btn = document.querySelector('button')
+
+
+// btn.addEventListener("click", () => {
+//     text.style.color = 'red'
+// })
+//    //   mouseover - btn ге мышка басылганда же мышка барганда
+//    //  ТЕКС   башка светке озгоруп калат
+//
+
+// btn.addEventListener("mouseover", () => {
+//         text.style.color = 'black'
+// })
+//
+// //  //  mouseleave  - h1ге мышка барганда башка свет куйот
+//
+// text.addEventListener("mouseleave", () => {
+//     text.style.color = 'red'
+// })
+
+// //  mousedown -  btn басылганда   h1  башка светке озгорот
+
+// btn.addEventListener("mousedown", () => {
+//     text.style.color = 'black'
+// })
+
+// // mouseup -  басып анан койуп жибергенде иштейт
+
+// text.addEventListener("mouseup", () => {
+//     text.style.color = 'red'
+// })
+
+
+
+
+ // text.style.color = 'blue'
+
+
+
